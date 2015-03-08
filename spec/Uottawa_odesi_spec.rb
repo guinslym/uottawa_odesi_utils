f628b64e1463063dc39c74d2b90d333ed6165c7c
Guinsly Mondesir <guinslym @gmail.com>
2015-03-08 19:42:00 -0400

    french first

--- spec/Uottawa_odesi_spec.rb
+++ spec/Uottawa_odesi_spec.rb
@@ -3,24 +3,24 @@ require 'spec_helper'
 describe UottawaOdesiUtils do
 
   # Crappy specs start here
-  describe ".IDD Documentation" do
+  describe ".DDI Documentation" do
      let(:lines) { UottawaOdesiUtils.retrieve_label_and_qstnlit(Dir.pwd+'/spec/data/esg-c-25.xml')}
-     let(:b_lines) { UottawaOdesiUtils.bilingual_files(Dir.pwd+'/spec/data/gss-c-25.xml',
-                                                Dir.pwd+'/spec/data/esg-c-25.xml')}
+     let(:b_lines) { UottawaOdesiUtils.bilingual_files(Dir.pwd+'/spec/data/esg-c-25.xml', 
+                                                       Dir.pwd+'/spec/data/gss-c-25.xml')}
 
     it "expecting it to be at least 79" do
       expect(lines.size).to be >= 79
     end
 
-    it "expecting it to return an array" do
+    it "expecting it to return an Array" do
       expect(lines).to be_instance_of Array
     end
 
-    it "each element is a Hash" do
+    it "expecting each element to be a Hash" do
       expect(lines.first).to be_instance_of Hash
     end
 
-    it "both file will have at least 79 elements" do
+    it "expecing both file (fr+en) will create a an Array of at least 79 elements" do
       expect(b_lines.size).to be >= 79
     end
 
